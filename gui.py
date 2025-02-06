a6f0489a0f5e12c8cdb7d3832240bc30ad3268c4
Manideep Pulla <manideeppulla1405@gmail.com>
2025-02-06 20:27:12 +0530

    Update gui.py

--- gui.py
+++ gui.py
@@ -50,9 +50,9 @@ def process_frame():
     if results.multi_hand_landmarks:
         for hand_landmarks in results.multi_hand_landmarks:
             mp_drawing.draw_landmarks(
-                frame,  # image to draw
-                hand_landmarks,  # model output
-                mp_hands.HAND_CONNECTIONS,  # hand connections
+                frame,  
+                hand_landmarks,  
+                mp_hands.HAND_CONNECTIONS,  
                 mp_drawing_styles.get_default_hand_landmarks_style(),
                 mp_drawing_styles.get_default_hand_connections_style())
 
